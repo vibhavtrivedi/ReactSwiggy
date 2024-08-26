eda804e134ba97f2dd1bec325fac4a9a64f2d6d5
vibhavtrivedi <vibhavkumar86@gmail.com>
2024-08-26 21:18:29 +0530

    clear cart using redux

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,11 +1,18 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import "./Cart.css";
 import ItemList from "../ResturantMenu/ItemList";
+import { deleteItems } from "../../utils/cartSlice";
 
 const CartComponent = () => {
   const cartStoreItems = useSelector((store) => store.cart.items);
+  const dispatch = useDispatch();
+  console.log("d", dispatch);
+  const handleClearCart = () => {
+    dispatch(deleteItems());
+  }
   return (
     <div className="cart-page">
+      <button onClick={handleClearCart} className="del-cart">Clear cart</button>
       <ItemList itemList={cartStoreItems} btn="delete" />
     </div>
   );
